7720a163669ca25241efae4fab13c623fdd472a4
Disha Honmane <119420363+ShivDeeksha@users.noreply.github.com>
2023-07-28 22:52:31 +0530

    Update swapWithoutTemp.py
    Program to swap values with and without using temporary variable

--- swapWithoutTemp.py
+++ swapWithoutTemp.py
@@ -1,14 +1,25 @@
-def swap(a,b):
-  a=a+b
-  b=a-b
-  a=a-b
-  print("a:",a)
-  print("b:",b)
+def swap(a, b):
+  a = a + b
+  b = a - b
+  a = a - b
+  print("a:", a)
+  print("b:", b)
 
-a,b=input("Enter two numbers: ").split()
-a=int(a)
-b=int(b)
-print("a:",a)
-print("b:",b)
-print("\nAfter Swap:")
-swap(a,b)
+
+def swap_temp(a, b):
+  temp = a
+  a = b
+  b = temp
+  print("a:", a)
+  print("b:", b)
+
+
+a, b = input("Enter two numbers: ").split()
+a = int(a)
+b = int(b)
+print("a:", a)
+print("b:", b)
+print("\nAfter Swap (Without using temp)")
+swap(a, b)
+print("\nAfter Swap (With using temp)")
+swap_temp(a,b)
