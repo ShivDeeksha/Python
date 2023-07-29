7ab481120e842a9a2dba1eecb47173ed9c383113
Disha Honmane <119420363+ShivDeeksha@users.noreply.github.com>
2023-07-29 22:08:49 +0530

    Create simpleInterest.py
    Program to calculate simple interest.

---
+++ simpleInterest.py
@@ -0,0 +1,8 @@
+def si(p,t,r):
+  si=(p*t*r)/100
+  print("Simple Interest:",si)
+
+p=float(input("Enter Principal: "))
+t=float(input("Enter Time: "))
+r=float(input("Enter Rate of Interest: "))
+si(p,t,r)
