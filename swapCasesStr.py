3067b0eff419040b416720e0a70c54cdc905e9e4
Disha Honmane <119420363+ShivDeeksha@users.noreply.github.com>
2023-08-08 11:36:30 +0530

    Create swapCasesStr.py

---
+++ swapCasesStr.py
@@ -0,0 +1,11 @@
+def swapCase(s):
+    res=''
+    for i in s:
+        if i.isupper():
+            res=res+i.lower()
+        elif i.islower():
+            res=res+i.upper()
+        else:
+            res=res+i
+    return res
+swapCase("Hello tHerE!!")
